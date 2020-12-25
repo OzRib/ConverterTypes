a3cd955f84dc62182cfa2493d9ca357b5f99c286
Ozeias <ozeiasnascimentoribeiro@gmail.com>
2020-12-25 19:31:29 -0300

    Updated examples

--- examples/CPP/ConvertTypes.cpp
+++ examples/CPP/ConvertTypes.cpp
@@ -4,7 +4,7 @@
 
 int main(){
 	char b[] = "01234a";
-	int a=StringToInt(b);
-	printf("\nResult: %d\n", a);
+	int a=StringToInt(b, sizeof(b));
+	printf("\nResult: %d\nsizeof: %ld", a, sizeof(b));
 	return 0;
 }
